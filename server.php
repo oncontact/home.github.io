4a37812154b05e2345e12f38fca76aa4b2e46521
Pabitra Hazarika <105353694+oncontact@users.noreply.github.com>
2022-05-18 16:21:55 +0530

    Add files via upload

---
+++ server.php
@@ -0,0 +1,12 @@
+<?php
+// mysqli
+$mysqli = new mysqli("example.com", "user", "password", "database");
+$result = $mysqli->query("SELECT 'Hello, dear MySQL user!' AS _message FROM DUAL");
+$row = $result->fetch_assoc();
+echo htmlentities($row['_message']);
+
+// PDO
+$pdo = new PDO('mysql:host=example.com;dbname=database', 'user', 'password');
+$statement = $pdo->query("SELECT 'Hello, dear MySQL user!' AS _message FROM DUAL");
+$row = $statement->fetch(PDO::FETCH_ASSOC);
+echo htmlentities($row['_message']);
